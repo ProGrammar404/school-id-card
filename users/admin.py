46f76c1936a9fb20d1d087aa7cc96680e47410fc
ProGrammar404 <sanyam.jainuc@gmail.com>
2026-05-23 16:49:57 +0530

    create the user app

---
+++ users/admin.py
@@ -0,0 +1,10 @@
+from .models import User
+from django.contrib import admin
+
+
+# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_filter = ("is_staff", "is_superuser", "is_active")
